1b129265f9a5c9e3c034fe3e0f879fe974ba59b1
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-07-12 14:11:57 -0700

    init commit/installed tailwindcss and daisyui and created table component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Table from './components/Table';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-center'>
+        LoL Custom Character Form
+      </h1>
+      <Table />
+    </>
+  );
+}
+
+export default App;
